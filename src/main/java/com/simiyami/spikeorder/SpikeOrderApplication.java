c04125c847180ed59b5430f1f56e666084bb7a08
hyot.ahn <hyot.ahn@kakaoenterprise.com>
2026-01-12 16:42:53 +0900

    init

---
+++ src/main/java/com/simiyami/spikeorder/SpikeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.simiyami.spikeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpikeOrderApplication.class, args);
+    }
+
+}
